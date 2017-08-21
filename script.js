c2e54f053f3d07c2a8317c3c06fd7d913e305367
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-08-21 13:15:06 -0400

    prevents illegal moves

--- script.js
+++ script.js
@@ -3,37 +3,52 @@ var selectedTower = undefined
 $('li').on('click', blockSelect)
 $('div').on('click', blockMove)
 
+// Block Selection
 function blockSelect(){
-  if (selectedBlock !== undefined) {selectedBlock.removeClass('selected')}
-
   clickedBlock = $(this)
+  if (isLegalSelection() === true) {
+    if (selectedBlock !== undefined) {selectedBlock.removeClass('selected')}
+    selectedBlock = $(this)
+    selectedBlock.attr('class', 'selected')
+  }
+}
+function isLegalSelection(){
   let clickedID = clickedBlock.attr('id')
   let firstID = clickedBlock.parent().children().eq(0).attr('id')
-
   if (clickedID === firstID) {
-    $(this).attr('class', 'selected')
-    selectedBlock = $(this)
+    return true
   }
-
 }
+
+// Block Movement
 function blockMove(){
   if (selectedBlock !== undefined) {
-    selectedTower = $(this).find('ol')
-    selectedBlock.prependTo(selectedTower)
+    selectedTower = $(this)
+    if (isLegalMove() === true) {
+      selectedBlock.prependTo(selectedTower)
+    }
   }
-  isIllegal()
+
   countMove()
   isWin()
 }
 
 
+function isLegalMove() {
+  let selectedID = selectedBlock.attr('id').slice(1)
+  if (selectedTower.children(0).attr('id') === undefined) {
+    towerTopID = 100
+  } else {
+    towerTopID = selectedTower.children(0).attr('id').slice(1)
+  }
+  if (selectedID < towerTopID) {
+    return true
+  }
 
+  // console.log(selectedID)
+  // console.log(towerTopID)
+  // test if selected block can move to tower
 
-
-
-
-function isIllegal(){
-// test if selected block can move to tower
 }
 function countMove(){
 // add 1 to tally of moves
