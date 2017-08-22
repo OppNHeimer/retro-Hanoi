eadf29dc46e0d758ccab3ce0b777c7c11a8d197a
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-08-22 15:24:05 -0400

    finished passable formatting and features

--- script.js
+++ script.js
@@ -2,22 +2,45 @@ createBlocks()
 var selectedBlock = undefined
 var selectedTower = undefined
 
-$('div').on('click', blockMove)
+$('.tower').on('click', blockMove)
 $('button').on('click', createBlocks)
+
 // Block Selection
 function blockSelect(){
   clickedBlock = $(this)
   if (isLegalSelection() === true) {
-    if (selectedBlock !== undefined) {selectedBlock.removeClass('selected')}
+    if (selectedBlock !== undefined) {blockClassReset()}
     selectedBlock = $(this)
-    selectedBlock.attr('class', 'selected')
+    stopHint()
+    blockClassSelected()
+    highlightTowers()
   }
 }
+// Change block to selected class
+function blockClassSelected() {selectedBlock.attr('class', 'selected')}
+
+// Resets block class to default
+function blockClassReset() {selectedBlock.removeClass('selected')}
+
+// Checks if clicked block is on top of tower
 function isLegalSelection(){
   let clickedID = clickedBlock.attr('id')
   let firstID = clickedBlock.parent().children().eq(0).attr('id')
   if (clickedID === firstID) {
     return true
+  }
+}
+
+// Resets tower class to default
+function towerClassReset() {$('.towerOption').attr('class', 'tower')}
+
+// highlights towers to move block to
+function highlightTowers(){
+  towerClassReset()
+  for (var i = 0; i < 3; i++) {
+    if ($('#game').children().eq(i).attr('id') !== clickedBlock.parent().attr('id')){
+      $(`#tower${i+1}`).attr('class', 'towerOption')
+    }
   }
 }
 
@@ -28,10 +51,13 @@ function blockMove(){
     if (isLegalMove() === true) {
       selectedBlock.prependTo(selectedTower)
       countMove()
+      highlightTowers()
     }
   }
   isWin()
 }
+
+// Checks if move is legal
 function isLegalMove() {
   let selectedID = selectedBlock.attr('id').slice(1)
   if (selectedTower.children(0).attr('id') === undefined) {
@@ -50,23 +76,28 @@ function countMove(){
   moveCount = parseInt($('#moveCount').text())
 }
 
-// Custom number of blocks
+// Resets game and creates tower of custom height
 function createBlocks() {
   $("li").remove()
+  selectedBlock = undefined
+  towerClassReset()
   numOfBlocks = $('input').val()
   blockLoop()
+  clickHint()
   bestPossible()
   $('li').on('click', blockSelect)
 }
+
+// Creates blocks
 function blockLoop() {
   for (let i = 0; i < numOfBlocks; i++) {
-    let blockWidth = 100 - i*(100/numOfBlocks)
-    let block = `<li id="b${numOfBlocks - i}"style="width: ${blockWidth}%">${numOfBlocks - i}</li>`
+    let blockWidth = 100 - (i + .5)*(80/numOfBlocks)
+    let block = `<li id="b${numOfBlocks - i}"style="width: ${blockWidth}%"></li>`
     $('#tower1').prepend(block)
   }
 }
 
-// WIN
+// Tests if user won
 function isWin(){
   let towerHeight2 = $("#tower2").children().length - 1
   let towerHeight3 = $("#tower3").children().length - 1
@@ -79,8 +110,23 @@ function isWin(){
   }
 }
 
-// calulate best possible score
+// Calulate the best possible score
 function bestPossible() {
   best = 2 ** numOfBlocks - 1
   $('#best').text(best)
+}
+
+// Gives user blinking hint
+function clickHint() {
+    hintBlock = $('#b1')
+    select = setInterval(hintClassSelected, 500)
+    reset = setInterval(hintClassReset, 1000)
+}
+function hintClassSelected() {hintBlock.attr('class', 'selected')}
+function hintClassReset() {hintBlock.removeClass('selected')}
+
+// Stops block from blinking
+function stopHint() {
+  clearInterval(select)
+  clearInterval(reset)
 }
